60990bb1b7b94c7550eb5c6857c4e4cfde763aed
Chris Tate <chris@zappjs.com>
2019-07-03 12:08:09 -0500

    sorts files

--- src/index.ts
+++ src/index.ts
@@ -11,21 +11,31 @@ program
   .command('generate')
   .action(async () => {
     // load zapp project
-    const project = require(`${process.cwd()}/.zapp/zapp`).default;
+    const files = require(`${process.cwd()}/.zapp/zapp`).default;
 
     console.log('Generated Files:');
 
-    Object.keys(project).forEach((fileName) => {
-      const fileContents = project[fileName];
-      const filePath = path.normalize(fileName);
-      const dirName = path.dirname(filePath);
-      if (!fs.existsSync(dirName)) {
-        fs.mkdirSync(dirName, { recursive: true });
-      }
-      fs.writeFileSync(filePath, fileContents);
+    Object.keys(files)
+      .sort((a, b) => {
+        if (a < b) {
+          return -1;
+        }
+        if (a > b) {
+          return 1;
+        }
+        return 0;
+      })
+      .forEach((fileName) => {
+        const fileContents = files[fileName];
+        const filePath = path.normalize(fileName);
+        const dirName = path.dirname(filePath);
+        if (!fs.existsSync(dirName)) {
+          fs.mkdirSync(dirName, { recursive: true });
+        }
+        fs.writeFileSync(filePath, fileContents);
 
-      console.log(`- ${filePath}`);
-    });
+        console.log(`- ${filePath}`);
+      });
   });
 
 program.parse(process.argv);
