6b6e93a34739a08fc8d26018d09681ed387e1f64
Chris Tate <chris@austincodeshop.com>
2021-10-25 01:32:44 -0500

    skips empty files

--- src/index.ts
+++ src/index.ts
@@ -4,43 +4,43 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as program from 'commander';
 
-program
-  .version('2.0.0-alpha.4');
+import * as pkg from '../package.json';
 
-program
-  .command('generate')
-  .action(async () => {
-    // load zapp project
-    const files = await require(`${process.cwd()}/.zapp/zapp`).default;
+program.version(pkg.version);
 
-    console.log('Generated Files:');
+program.command('generate').action(async () => {
+  // load zapp project
+  const files = await require(`${process.cwd()}/.zapp/zapp`).default;
 
-    Object.keys(files)
-      .sort((a, b) => {
-        if (a < b) {
-          return -1;
-        }
-        if (a > b) {
-          return 1;
-        }
-        return 0;
-      })
-      .forEach(async (fileName) => {
-        const fileContents = await files[fileName];
-        const filePath = path.normalize(fileName);
-        const dirName = path.dirname(filePath);
+  console.log('Generated Files:');
 
-        if (typeof fileContents !== 'string') {
-          console.log(`- ${filePath} (skipped)`);
-          return;
-        }
-        if (!fs.existsSync(dirName)) {
-          fs.mkdirSync(dirName, { recursive: true });
-        }
-        fs.writeFileSync(filePath, fileContents);
+  Object.keys(files)
+    .sort((a, b) => {
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+    })
+    .forEach(async (fileName) => {
+      const fileContents = await files[fileName];
+      const filePath = path.normalize(fileName);
+      const dirName = path.dirname(filePath);
 
-        console.log(`- ${filePath}`);
-      });
-  });
+      if (typeof fileContents !== 'string') {
+        console.log(`- ${filePath} (skipped)`);
+        return;
+      }
+
+      if (!fs.existsSync(dirName)) {
+        fs.mkdirSync(dirName, { recursive: true });
+      }
+      fs.writeFileSync(filePath, fileContents);
+
+      console.log(`- ${filePath}`);
+    });
+});
 
 program.parse(process.argv);
